013e5edb06f03d980d3be47cb4aa1f14f9b2c426
lixuan <1318753541@qq.com>
2023-12-27 22:35:10 +0800

    udpate

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,2 @@
-// export const request_prefix = "http://localhost:8501/zorroe"
-export const request_prefix = "http://110.42.210.221:8080/zorroe"
+export const request_prefix = "https://zorroexpress.vercel.app"
+// export const request_prefix = "http://localhost:9000"
