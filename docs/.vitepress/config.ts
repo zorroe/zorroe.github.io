87dbeb9dc841ce670220bd0257dbb7a294f60e75
lixuan <1318753541@qq.com>
2024-01-19 20:34:29 +0800

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: '/',
-
   title: 'zorroe',
   description: "zorroe's space",
+  lastUpdated: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     logo: 'logo.svg',
@@ -13,7 +13,6 @@ export default defineConfig({
       { text: 'Home', link: '/' },
       { text: 'Examples', link: '/markdown-examples' },
     ],
-
     sidebar: [
       {
         text: 'Examples',
@@ -23,20 +22,31 @@ export default defineConfig({
         ],
       },
     ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/zorroe' },
-    ],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/zorroe' }],
     search: {
       provider: 'local',
     },
     lastUpdated: {
       text: '最后更新于',
       formatOptions: {
-        dateStyle: 'full',
-        timeStyle: 'medium'
-      }
-    }
+        dateStyle: 'short',
+        timeStyle: 'medium',
+      },
+    },
+    returnToTopLabel: '回到顶部',
+    darkModeSwitchTitle: '切换到深色模式',
+    lightModeSwitchTitle: '切换到浅色模式',
+    docFooter: {
+      prev: '上一页',
+      next: '下一页',
+    },
+    outline: {
+      label: '页面导航',
+    },
+    editLink: {
+      pattern:
+        'https://github.com/zorroe/zorroe.github.io/edit/main/docs/:path',
+      text: '在Github上编辑此页',
+    },
   },
-  lastUpdated: true,
 })
