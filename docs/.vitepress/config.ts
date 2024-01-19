afe816b8c8be55a3fd423b56ede9091872d87460
lixuan <1318753541@qq.com>
2024-01-19 20:56:24 +0800

    update 中文

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     logo: 'logo.svg',
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' },
+      { text: '首页', link: '/' },
+      { text: '案例', link: '/markdown-examples' },
     ],
     sidebar: [
       {
@@ -25,6 +25,25 @@ export default defineConfig({
     socialLinks: [{ icon: 'github', link: 'https://github.com/zorroe' }],
     search: {
       provider: 'local',
+      options: {
+        translations: {
+              
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档',
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            backButtonTitle: '返回',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭'
+            },
+          },
+        },
+      },
     },
     lastUpdated: {
       text: '最后更新于',
