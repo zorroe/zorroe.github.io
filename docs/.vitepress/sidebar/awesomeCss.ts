de54137a381c1971677413988042013bafa72be6
lixuan <1318753541@qq.com>
2024-02-17 17:20:21 +0800

    udpate

--- docs/.vitepress/sidebar/awesomeCss.ts
+++ docs/.vitepress/sidebar/awesomeCss.ts
@@ -4,7 +4,13 @@ export const awesomeCssSideBar: () => DefaultTheme.SidebarItem[] = () => {
   return [
     {
       text: '神奇css',
-      items: [{ text: '简介', link: 'intro' }],
+      items: [
+        { text: '简介', link: 'intro' },
+        {
+          text: '左边框的多种实现方式',
+          link: 'css001',
+        },
+      ],
     },
   ]
 }
