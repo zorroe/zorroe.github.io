2e264bca203f111a144b6b7210055f62d7812042
lixuan <1318753541@qq.com>
2024-03-15 14:34:52 +0800

    update

--- docs/.vitepress/sidebar/other.ts
+++ docs/.vitepress/sidebar/other.ts
@@ -6,6 +6,7 @@ export const otherSideBar: () => DefaultTheme.SidebarItem[] = () => {
       text: '记',
       items: [
         { text: 'MySQL同步数据到ES', link: 'mysql2es' },
+        { text: '一些代码切片', link: 'codeSnippet'}
       ],
     },
   ]
---
+++ docs/other/codeSnippet.md
@@ -0,0 +1,5 @@
+- js 给数字加千分符的方法
+
+```javascript
+const number = new Intl.NumberFormat().format(number)
+```
